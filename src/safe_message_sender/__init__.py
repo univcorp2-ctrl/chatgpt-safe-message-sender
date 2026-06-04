5ba2b2140bcf2c692c7f27659d6c813ac976fb24
univcorp2-ctrl <univcorp2@gmail.com>
2026-06-04 23:43:53 +0900

    Add package init

---
+++ src/safe_message_sender/__init__.py
@@ -0,0 +1,5 @@
+"""Safe OpenAI Responses API message sender."""
+
+from safe_message_sender.client import OpenAIResponseClient, ResponseResult
+
+__all__ = ["OpenAIResponseClient", "ResponseResult"]
